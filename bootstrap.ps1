e06745bf45a93ce5cad1c18b98c0a9043f5f1b73
cuinixam <22344617+cuinixam@users.noreply.github.com>
2023-07-02 18:12:49 +0200

    feat: update bootstrap script

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,16 +1,25 @@
 <#
 .DESCRIPTION
-    Bootstrap script for installing scoop and the configured packages in 'scoopfile.json'.
-    This script was created based on https://github.com/avengineers/bootstrap
+    Script to install python and start the build.py.
 #>
-
-[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSReviewUnusedParameter', '', Justification = 'Used in global scope in Main function')]
-
 
 # About preference variables: https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_preference_variable
 # Stop execution on first non-terminating error (an error that doesn't stop the cmdlet processing)
 $ErrorActionPreference = "Stop"
 
+###################################################################################################
+# Configuration
+###################################################################################################
+$config = @{
+    # Required Python version - major.minor (e.g. 3.11)
+    pythonVersion = "3.10"
+    # Scoop install script
+    scoopInstaller = "https://raw.githubusercontent.com/ScoopInstaller/Install/master/install.ps1"
+}
+
+###################################################################################################
+# Utility functions
+###################################################################################################
 
 Function Edit-Env {
     # workaround for GithubActions
@@ -50,42 +59,71 @@ Function Invoke-CommandLine {
 }
 
 Function Install-Scoop {
-    if (Test-Path -Path 'scoopfile.json') {
-        Write-Output "File 'scoopfile.json' found, installing scoop and running 'scoop import' ..."
-        # Initial Scoop installation
-        if (-Not (Get-Command 'scoop' -ErrorAction SilentlyContinue)) {
-            Invoke-RestMethod 'https://raw.githubusercontent.com/xxthunder/ScoopInstall/master/install.ps1' -outfile "$PSScriptRoot\bootstrap.scoop.ps1"
-            if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-                & $PSScriptRoot\bootstrap.scoop.ps1 -RunAsAdmin
-            }
-            else {
-                & $PSScriptRoot\bootstrap.scoop.ps1
-            }
-            Edit-Env
+    # Initial Scoop installation
+    if (-Not (Get-Command 'scoop' -ErrorAction SilentlyContinue)) {
+        $tempFile = New-TemporaryFile
+        Invoke-RestMethod $config.scoopInstaller -OutFile $tempFile.FullName
+        if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+            & $tempFile.FullName -RunAsAdmin
         }
-
-        # install needed tools
-        Invoke-CommandLine "scoop update"
-        Invoke-CommandLine "scoop install lessmsi" -Silent $true
-
-        # Some old tweak to get 7zip installed correctly
-        Invoke-CommandLine "scoop config use_lessmsi $true" -Silent $true
-
-        # avoid deadlocks while updating scoop buckets
-        Invoke-CommandLine "scoop config autostash_on_conflict $true" -Silent $true
-
-        # some prerequisites to install other packages
-        Invoke-CommandLine "scoop install 7zip" -Silent $true
-        Invoke-CommandLine "scoop install innounp" -StopAtError $false -Silent $true
-        Invoke-CommandLine "scoop install dark" -Silent $true
-        Invoke-CommandLine "scoop import scoopfile.json"
+        else {
+            & $tempFile.FullName
+        }
         Edit-Env
+        Remove-Item $tempFile.FullName
     }
-    else {
-        Write-Output "File 'scoopfile.json' not found, skipping Scoop setup."
-    }
+
+    # Install needed tools
+    Invoke-CommandLine "scoop update"
+    Invoke-CommandLine "scoop install lessmsi" -Silent $true
+
+    # Some old tweak to get 7zip installed correctly
+    Invoke-CommandLine "scoop config use_lessmsi $true" -Silent $true
+
+    # Avoid deadlocks while updating scoop buckets
+    Invoke-CommandLine "scoop config autostash_on_conflict $true" -Silent $true
+
+    # Some prerequisites to install other packages
+    Invoke-CommandLine "scoop install 7zip" -Silent $true
+    Invoke-CommandLine "scoop install innounp" -StopAtError $false -Silent $true
+    Invoke-CommandLine "scoop install dark" -Silent $true
+    Edit-Env
 }
 
-## start of script
-Install-Scoop
-## end of script
+###################################################################################################
+# Main
+###################################################################################################
+
+# python executable name
+$python = "python" + $config.pythonVersion.Replace(".", "")
+
+# Check if scoop is installed
+$scoopPath = (Get-Command scoop -ErrorAction SilentlyContinue).Source
+if ($scoopPath -eq $null) {
+    Write-Output "Scoop not found. Trying to install scoop ..."
+    Install-Scoop
+} else {
+    Write-Output "Found scoop under $scoopPath."
+}
+# Check if python is installed
+$pythonPath = (Get-Command $python -ErrorAction SilentlyContinue).Source
+if ($pythonPath -eq $null) {
+    Write-Output "$python not found. Try to install $python via scoop ..."
+    # Install python
+    Invoke-CommandLine "scoop install $python"
+    # Check if python is installed
+} else {
+    Write-Output "$python found in $pythonPath"
+    # Extract the directory of python exe file and add it to PATH. It needs to be the first entry in PATH
+    # such that this version is used when the user calls python and not python311
+    $pythonDir = [System.IO.Path]::GetDirectoryName($pythonPath)
+    Write-Output "Adding $pythonDir to PATH"
+    $Env:Path += ";$pythonDir"
+}
+
+# Call the build.py if it exists with all provided arguments
+$buildScript = Join-Path $PSScriptRoot "bootstrap.py"
+if (Test-Path $buildScript) {
+    Write-Output "Calling $buildScript ..."
+    & $python $buildScript $args
+}
