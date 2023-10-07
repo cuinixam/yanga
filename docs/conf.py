8386089f3cb788753d0759d7a503e6550227b6b2
cuinixam <22344617+cuinixam@users.noreply.github.com>
2023-10-07 20:16:57 +0200

    docs: use dark mode

--- docs/conf.py
+++ docs/conf.py
@@ -69,6 +69,7 @@ source_suffix = [
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+extensions.append("sphinx_rtd_dark_mode")
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
