9f924e9fc9e66f7ec52bbe780f188bff6a9929f0
cuinixam <22344617+cuinixam@users.noreply.github.com>
2024-02-23 19:54:11 +0100

    fix: only build executable for tagged commits

--- build_exe.ps1
+++ build_exe.ps1
@@ -3,22 +3,24 @@
 #  Archive the executable as yanga.windows.amd64.<tag>.zip
 #  The <tag> is the latest git tag on remote origin.
 
-# Usage: .\build_exe.ps1
-# Example: .\build_exe.ps1
-
 # Fetch the latest git tag from remote origin
 git fetch --tags
 
-# Get the latest git tag
-$tag = git describe --tags --abbrev=0
-Write-Host "Latest git tag: $tag"
+# Get the tag of the current commit (HEAD)
+$tag = git tag --contains HEAD
+Write-Host "Tag of the current commit: $tag"
 
-# Build the executable by running pyinstaller with poetry from the .venv/Scripts/poetry
-$poetry = ".\.venv\Scripts\poetry"
-& $poetry run pyinstaller build_exe.spec
+# Check if the tag matches the version pattern v*.*.*
+if ($tag -match "v\d+\.\d+\.\d+") {
+    # Build the executable by running pyinstaller with poetry from the .venv/Scripts/poetry
+    $poetry = ".\.venv\Scripts\poetry"
+    & $poetry run pyinstaller build_exe.spec
 
-# Archive the executable
-$tag = $tag -replace "v", ""
-$zip = "dist\yanga-$tag-windows-amd64.zip"
-Compress-Archive -Path "dist\yanga.exe" -DestinationPath $zip
-Write-Host "Archive created: $zip"
+    # Archive the executable
+    $tag = $tag -replace "v", ""
+    $zip = "dist\yanga-$tag-windows-amd64.zip"
+    Compress-Archive -Path "dist\yanga.exe" -DestinationPath $zip
+    Write-Host "Archive created: $zip"
+} else {
+    Write-Host "The current commit does not have a version tag v*.*.*"
+}
